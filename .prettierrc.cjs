314ff51b8004a00e6e476f9a01404f7775283aa3
Barbaro Javier <barbaro.valmaseda@gmail.com>
2023-07-06 21:25:44 +0400

    feat: create title component

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,19 +1,9 @@
 module.exports = {
-	plugins: [require.resolve('prettier-plugin-astro')],
 	printWidth: 100,
 	semi: false,
 	singleQuote: true,
 	jsxSingleQuote: true,
 	tabWidth: 2,
 	trailingComma: 'none',
-	useTabs: true,
-	overrides: [
-		{
-			files: '*.astro',
-			options: {
-				parser: 'astro'
-			}
-		}
-	],
-	endOfLine: 'lf'
+	useTabs: true
 }
